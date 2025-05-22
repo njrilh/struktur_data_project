ddf71f526014164f14f36e5ced28df33001c3f4b
njrilh <136064036+njrilh@users.noreply.github.com>
2025-05-22 19:25:42 +0700

    Update aisubg.py

--- aisubg.py
+++ aisubg.py
@@ -3,12 +3,13 @@ arrA = ["a", "b", "c", "d", "a"]
 arrB = ["b", "c", "d", "a", "c"]
 
 # graph turunan
-inp1 = ["a", "b", "a"]
-inp2 = ["b", "c", "c"]
+inp1 = ["a", "b", ]
+inp2 = ["b", "c", ]
 
 # tampungan
 edgeAsli = []
 edgeTurunan = []
+is_spanning = True
 
 # bikin jadi bolak balik
 for i in range(len(arrA)):
@@ -18,20 +19,17 @@ for i in range(len(inp1)):
     edgeTurunan.append((inp1[i], inp2[i]))
     edgeTurunan.append((inp2[i], inp1[i]))
 
-# Cek apakah turunan adalah subgraph (semua edge-nya ada di graph asli)
-is_subgraph = True
+# cek spanning atau subg
+if (inp2[-1], inp1[0]) in edgeAsli :
+    if inp2[-1] != inp1[0] :
+        is_spanning = False
+
+if len(inp1) < 2 :
+    is_spanning = True
+
 for i in range (len(edgeTurunan)):
     if edgeTurunan[i] not in edgeAsli:
-        is_subgraph = False
+        is_spanning = False
         break
 
-# Cek simpul dari graf asli dan graf turunan
-simpulAsli = set(arrA + arrB)
-simpulTurunan = set(inp1 + inp2)
-
-is_spanning = False
-if is_subgraph and simpulTurunan == simpulAsli:
-    if len(edgeTurunan) // 2 == len(simpulTurunan) - 1:  # spanning tree: |E| = |V| - 1
-        is_spanning = True
-
-print("Ini Spaanning Tree" if is_spanning else "Ini Subgraph" if is_subgraph else "Ini bukan spanning dan bukan sub")
+print("Ini Spaanning Tree" if is_spanning else "Ini Subgraph")
