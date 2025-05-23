c7d7d4db6e6ab421128d49f7fcf9b3e55fd349e2
njrilh <136064036+njrilh@users.noreply.github.com>
2025-05-23 14:38:28 +0700

    Update strd.py

--- strd.py
+++ strd.py
@@ -4,25 +4,24 @@ inp2 = []
 eu = False
 mulG = False
 
-# Inputan ------------------------------------------------
+# Inputan
 inpe = int(input("Masukan ada berapa ruas(e) = "))
 for i in range (inpe) :
     in1 = str(input(f"Ruas e{i+1} dari simpul = "))
     in2 = str(input("ke simpul = "))
     if len(inp1)+len(inp2) == 0 :
-        inp1.append(in1)
-        inp2.append(in2)
+        inp1.append(in1), inp2.append(in2)
     elif inp2[i-1] == in2 :
-        inp1.append(in2)
-        inp2.append(in1)
+        inp1.append(in2), inp2.append(in1)
     else :
-        inp1.append(in1)
-        inp2.append(in2)
+        inp1.append(in1), inp2.append(in2)
 #----------------------------------------------------------
 
+# identifikasi derajat graph
 totalSimpul = list(set(inp1 + inp2))
 derajatGraph = len(totalSimpul) * 2
 print(f"{inp1}\n{inp2}\nOrder = {len(totalSimpul)}\nSize = {inpe}\nDerajat Graph = {derajatGraph}")
+# ----------------------------------------------------------
 
 # identfikasi Euler
 for i in  range (len(totalSimpul)) :
@@ -30,11 +29,12 @@ for i in  range (len(totalSimpul)) :
         eu = True
     else : break
 print("Ini Euler" if eu else " Ini bukan Euler")
+# ----------------------------------------------------------
 
 # identfksi multigraph
 for i in range (len(inp1)-1) :
     if (inp1[i]) == (inp2[i]) or ((inp1[i] == inp1[i+1]) and (inp2[i] == inp2[i+1])) or ((inp2[i] ==inp1[i+1] and inp1[i] == inp2[i+1])) :
         mulG = True
     else: break
-
 print("Ini Multigraph" if mulG else "Ini Simple Graph")
+# -----------------------------------------------------------
