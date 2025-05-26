e0f9564f224939d27cf06b28bd107c149f104e09
njrilh <136064036+njrilh@users.noreply.github.com>
2025-05-26 17:28:10 +0700

    Update and rename mstcrt.py to dmstcrt.py

--- dmstcrt.py
+++ dmstcrt.py
@@ -2,42 +2,28 @@
 a = ["a", "b", "c", "d", "b"]
 b = ["b", "c", "d", "a", "d"]
 v = [1, 4, 2, 1, 4]
-
-awal = str(input("Masukan simpul awal = "))
-akhir = str(input("Masukan simpul akhir = "))
-
-# tampungan
+# input dan tampungan
+awal = str(input("Masukan simpul awal = ")).lower()
+akhir = str(input("Masukan simpul akhir = ")).lower()
 jalur = []
 vJalur = []
 hasil = []
-
 # mencari semua jalur dari awal ke akhir
 def cari_jalur(posisi, total, dilewati):
     if posisi == akhir:
-        vJalur.append(total)
-        jalur.append(dilewati)
+        vJalur.append(total), jalur.append(dilewati)
         return
-
     for i in range(len(a)):
         if a[i] == posisi and b[i] not in dilewati:
             cari_jalur(b[i], total + [v[i]], dilewati + [b[i]])
         elif b[i] == posisi and a[i] not in dilewati:
             cari_jalur(a[i], total + [v[i]], dilewati + [a[i]])
-
 # menjumlahkan seluruh value dari tiap jalur ditemukan
 def jumHasil() :
      for i in range (len(vJalur)) :
          hasil.append(sum(vJalur[i]))
-
 # eksekusi function
 cari_jalur(awal, [], [awal])
 jumHasil()
-
-# ctak 
-print("Hasil pencarian semua ruas = ", jalur)
-print("Hasil value pencarian semua ruas = ", vJalur)
-print("Semua total bobot dari", awal, "ke", akhir, "= ", hasil)
-if len(hasil) > 0:
-    print("Minimum Spanning Tree= ", min(hasil), "\nCritical Path = ", max(hasil))
-else:
-    print("Tidak ada jalur ditemukan.")
+print(f"Hasil pencarian semua ruas = {jalur} \nHasil value pencarian semua ruas = {vJalur}\nSemua total bobot dari {awal} ke {akhir} = {hasil}")
+print(f"Minimum Spanning Tree = {min(hasil)}\nCritical Path = {max(hasil)}" if len(hasil) > 0 else "Tidak ada jalur ditemukan.")
