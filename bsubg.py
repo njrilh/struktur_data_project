890c0441f2ea76b002422e39f63221d04293a06f
njrilh <136064036+njrilh@users.noreply.github.com>
2025-05-26 17:20:54 +0700

    Update and rename subg.py to bsubg.py

--- bsubg.py
+++ bsubg.py
@@ -1,15 +1,13 @@
 # graph asli
 arrA = ["a", "b", "c", "d", "a"]
 arrB = ["b", "c", "d", "a", "c"]
-
-# graph turunan
+# tamungan dan inputan
 inp1 = []
 inp2 = []
-
 edg = int(input("Ada berapa rusa dalam graph turunan ? = "))
 for i in range (edg) :
-    in1 = str(input(f"Ruas dari simpul = "))
-    in2 = str(input("ke simpul = "))
+    in1 = str(input("Ruas dari simpul = ")).lower()
+    in2 = str(input("ke simpul = ")).lower()
     if len(inp1)+len(inp2) == 0 :
         inp1.append(in1), inp2.append(in2)
     elif inp2[i-1] == in2 :
@@ -17,31 +15,23 @@ for i in range (edg) :
     else :
         inp1.append(in1), inp2.append(in2)
 print(f"{inp1}\n{inp2}")
-
 # tampungan
 edgeAsli = []
 edgeTurunan = []
 is_spanning = True
-
 # bikin jadi bolak balik
 for i in range(len(arrA)):
-    edgeAsli.append((arrA[i], arrB[i]))
-    edgeAsli.append((arrB[i], arrA[i]))
+    edgeAsli.append((arrA[i], arrB[i])), edgeAsli.append((arrB[i], arrA[i]))
 for i in range(len(inp1)):
-    edgeTurunan.append((inp1[i], inp2[i]))
-    edgeTurunan.append((inp2[i], inp1[i]))
-
+    edgeTurunan.append((inp1[i], inp2[i])), edgeTurunan.append((inp2[i], inp1[i]))
 # cek spanning atau subg
-if (inp2[-1], inp1[0]) in edgeAsli :
-    if inp2[-1] != inp1[0] :
-        is_spanning = False
-
-if len(inp1) < 2 :
-    is_spanning = True
-
 for i in range (len(edgeTurunan)):
     if edgeTurunan[i] not in edgeAsli:
         is_spanning = False
         break
-
+if len(inp1) < 2 :
+    is_spanning = True
+if (inp2[-1], inp1[0]) in edgeAsli :
+    if inp2[-1] != inp1[0] :
+        is_spanning = False
 print("Ini Spaanning Tree" if is_spanning else "Ini Subgraph")
