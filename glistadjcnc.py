84062be4830d336a8b4b2036e4255ea81a4099e9
njrilh <136064036+njrilh@users.noreply.github.com>
2025-05-31 17:04:20 +0700

    Update glistadjcnc.py

--- glistadjcnc.py
+++ glistadjcnc.py
@@ -1,24 +1,32 @@
 size = int(input("Berapa simpul? = "))
 node_names = input("Masukkan nama-nama simpul (pisahkan spasi): ").split()
 name_to_index = {}
-for i in range(len(node_names)):
-    name_to_index[node_names[i]] = i
-adj_list = [[] for _ in range(size)]
+for i in range(len(node_names)) :
+    name_to_index[node_names[i]] = i 
+matrix = [[0] * size for _ in range(size)]
+list = [[]for _ in range (size)]
 def add_edge(u, v):
     u_idx = name_to_index[u]
     v_idx = name_to_index[v]
-    adj_list[u_idx].append(v_idx)
-    adj_list[v_idx].append(u_idx)
-def print_adj_list():
+    matrix[u_idx][v_idx] = 1
+    matrix[v_idx][u_idx] = 1
+    list[u_idx].append(v_idx)
+    list[v_idx].append(u_idx)
+def print_matrix():
+    print("matriks adjc")
+    print("  ", " ".join(node_names))
+    for i, row in enumerate(matrix):
+        print(node_names[i], row)
+def print_list():
+    print("list adjc")
     for i in range(size):
         simpul = node_names[i]
-        tetangga = [node_names[j] for j in adj_list[i]]
+        tetangga = [node_names[j] for j in list[i]]
         print(f"{simpul} --> {', '.join(tetangga)}")
 ruas = int(input("Masukkan total berapa ruas (edge)? = "))
 for _ in range(ruas):
     inp1 = input("Masukkan simpul asal (huruf): ").strip()
     inp2 = input("Masukkan simpul tujuan (huruf): ").strip()
     add_edge(inp1, inp2)
-print(node_names, name_to_index, adj_list)
-print("\nAdjacency List:")
-print_adj_list()
+print_matrix()
+print_list()
