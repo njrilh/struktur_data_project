d1c26dcb61a46045a209f1436eb0d8b9ae1c1947
njrilh <136064036+njrilh@users.noreply.github.com>
2025-06-01 17:19:03 +0700

    Update gmatrikslistadjc.py

--- gmatrikslistadjc.py
+++ gmatrikslistadjc.py
@@ -1,8 +1,9 @@
 size = int(input("berapa simpul? = "))
-simpul = input("masukkan nama2 simpul (pisahkan spasi): ").split()
+simpul = input(str("masukkan nama2 simpul (pisahkan spasi): ")).split()
 simpulIdx = {}
 for i in range(len(simpul)) :
     simpulIdx[simpul[i]] = i 
+print(f"{simpul}\n{simpulIdx}")
 matrix = [[0] * size for _ in range(size)]
 list = [[]for _ in range (size)]
 def addRuas(u, v):
@@ -12,20 +13,20 @@ def addRuas(u, v):
     matrix[vIdx][uIdx] = 1
     list[uIdx].append(vIdx)
     list[vIdx].append(uIdx)
+    print(f'{matrix}\n{list}')
 def printMatrix():
     print("matriks adjc", "\n  ", " ".join(simpul))
-    for i, row in enumerate(matrix):
-        print(simpul[i], row)
+    for i in range(len(matrix)):
+        print(simpul[i], matrix[i])
 def printList():
     print("list adjc")
     for i in range(size):
-        smpl = simpul[i]
         tetangga = [simpul[j] for j in list[i]]
-        print(f"{smpl} --> {', '.join(tetangga)}")
+        print(f"{simpul[i]} --> {', '.join(tetangga)}")
 ruas = int(input("masukkan total berapa ruas ? = "))
 for _ in range(ruas):
-    inp1 = input("masukkan dari simpul : ").strip()
-    inp2 = input("masukkan ke simpul : ").strip()
+    inp1 = str(input("masukkan dari simpul : ")).strip()
+    inp2 = input(str("ke simpul : ")).strip()
     addRuas(inp1, inp2)
 printMatrix()
 printList()
